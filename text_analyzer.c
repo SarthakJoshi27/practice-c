d3e5d3228b94a5771b13509ac5fc9b4e6c5182f3
SarthakJoshi27 <102740140+SarthakJoshi27@users.noreply.github.com>
2025-02-19 22:11:46 +0530

    Update text_analyzer.c

--- text_analyzer.c
+++ text_analyzer.c
@@ -34,10 +34,19 @@ int vowels(char *ch){
     return count;
 }
 
+// int consonents(char *ch){
+//     int count = 0, i = 0;
+//     while(ch[i] != '\0'){
+//         if (ch[i] == 'b' || ch[i] == 'c' || ch[i] == 'd' || ch[i] == 'f' || ch[i] == 'g' || ch[i] == 'h' || ch[i] == 'j' || ch[i] == 'k' || ch[i] == 'l' || ch[i] == 'm' || ch[i] == 'n' || ch[i] == 'p' || ch[i] == 'q' || ch[i] == 'r' || ch[i] == 's' || ch[i] == 't' || ch[i] == 'v' || ch[i] == 'w' || ch[i] == 'x' || ch[i] == 'y' || ch[i] == 'z' || ch[i] == 'B' || ch[i] == 'C' || ch[i] == 'D' || ch[i] == 'F' || ch[i] == 'G' || ch[i] == 'H' || ch[i] == 'J' || ch[i] == 'K' || ch[i] == 'L' || ch[i] == 'M' || ch[i] == 'N' || ch[i] == 'P' || ch[i] == 'Q' || ch[i] == 'R' || ch[i] == 'S' || ch[i] == 'T' || ch[i] == 'V' || ch[i] == 'W' || ch[i] == 'X' || ch[i] == 'Y' || ch[i] == 'Z') count++;
+//         i++;
+//     }
+//     return count;
+// }
+
 int consonents(char *ch){
     int count = 0, i = 0;
     while(ch[i] != '\0'){
-        if (ch[i] == 'b' || ch[i] == 'c' || ch[i] == 'd' || ch[i] == 'f' || ch[i] == 'g' || ch[i] == 'h' || ch[i] == 'j' || ch[i] == 'k' || ch[i] == 'l' || ch[i] == 'm' || ch[i] == 'n' || ch[i] == 'p' || ch[i] == 'q' || ch[i] == 'r' || ch[i] == 's' || ch[i] == 't' || ch[i] == 'v' || ch[i] == 'w' || ch[i] == 'x' || ch[i] == 'y' || ch[i] == 'z' || ch[i] == 'B' || ch[i] == 'C' || ch[i] == 'D' || ch[i] == 'F' || ch[i] == 'G' || ch[i] == 'H' || ch[i] == 'J' || ch[i] == 'K' || ch[i] == 'L' || ch[i] == 'M' || ch[i] == 'N' || ch[i] == 'P' || ch[i] == 'Q' || ch[i] == 'R' || ch[i] == 'S' || ch[i] == 'T' || ch[i] == 'V' || ch[i] == 'W' || ch[i] == 'X' || ch[i] == 'Y' || ch[i] == 'Z') count++;
+        if(!(ch[i] == 'a' || ch[i] == 'e' || ch[i] == 'i' || ch[i] == 'o' || ch[i] == 'u' || ch[i] == 'A' || ch[i] == 'E' || ch[i] == 'I' || ch[i] == 'O' || ch[i] == 'U' || ch[i] == ' ')) count++;
         i++;
     }
     return count;
@@ -54,7 +63,7 @@ int main(){
         if(sw == 0) break;
 
         scanf("%d", &ch);
-
+        while(getchar()!= '\n');
         switch(ch){
             case 1:
             printf("\nEnter a full line for word count: ");
@@ -64,13 +73,13 @@ int main(){
 
             case 2:
             printf("Enter a full line for vowel count : ");
-            scanf("%s", str);
+            fgets(str, sizeof(str), stdin);
             printf("\n number of vowels : %d\n", vowels(str));
             break;
 
             case 3:
             printf("Enter a full line for consonents count : ");
-            scanf("%[^\n]", str);
+            fgets(str, sizeof(str), stdin);
             printf("\n number of words : %d\n", consonents(str));
             break;
 
